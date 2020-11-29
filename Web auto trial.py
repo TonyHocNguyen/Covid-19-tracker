ba58f0bb31782bfeaf6da817f0e0801717edb6e3
TonyHocNguyen <hoc.nguyen.190028@student.fulbright.edu.vn>
2020-11-29 21:31:43 +0700

    first commit for web automation

---
+++ Web auto trial.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+#from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+driver.get('https://worldhealthorg.shinyapps.io/covid/')
+
+time.sleep(2)
+
+acceptbutton = driver.find_element_by_xpath('//*[@id="closeModal"]')
+acceptbutton.click()
+
+country_tab = driver.find_element_by_xpath('//*[@id="tabs"]/li[4]/a')
+country_tab.click()
+
+country = input(str("Enter the country name: "))
+
+SearchBox = driver.find_element_by_xpath('//input[@id="singlecountry_header"]/div/div/div/div[1]')
+SearchBox.send_keys(country)
+SearchBox.submit()
+
+#SearchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+#SearchButton.click()
