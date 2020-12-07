44ee822abe7b78035394e059cd5b84d6eea0376b
SkepticalYouth <chamber2002@gmail.com>
2020-12-07 22:00:09 +0700

    secverse

--- Web auto trial.py
+++ Web auto trial.py
@@ -1,24 +1,28 @@
 from selenium import webdriver
 import time
 #from selenium.webdriver.common.keys import Keys
+def get_data():
 
-driver = webdriver.Chrome()
+    driver = webdriver.Chrome()
 
-driver.get('https://worldhealthorg.shinyapps.io/covid/')
+    driver.get('https://www.kayak.com/travel-restrictions?fbclid=IwAR2YLyQuyq4PDBDX1s5Z-k_K-uOiEPkevwuylWVXbU08r5UsWmbs-CICOR4')
+     
+    time.sleep(2)
+    """
+    terms_and_condition = driver.find_element_by_xpath('//*[@id="captcha_agree"]')
+    terms_and_condition.click()
 
-time.sleep(2)
+    captcha = driver.find_element_by_xpath('//*[@id="recaptcha-anchor"]/div[1]')
+    captcha.click()
 
-acceptbutton = driver.find_element_by_xpath('//*[@id="closeModal"]')
-acceptbutton.click()
+    country = input(str("Enter the country name: "))
 
-country_tab = driver.find_element_by_xpath('//*[@id="tabs"]/li[4]/a')
-country_tab.click()
-
-country = input(str("Enter the country name: "))
-
-SearchBox = driver.find_element_by_xpath('//input[@id="singlecountry_header"]/div/div/div/div[1]')
-SearchBox.send_keys(country)
-SearchBox.submit()
-
-#SearchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
-#SearchButton.click()
+    SearchBox = driver.find_element_by_xpath('//input[@id="singlecountry_header"]/div/div/div/div[1]')
+    SearchBox.send_keys(country)
+    SearchBox.submit()
+    
+    #SearchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+    #SearchButton.click()
+    """
+    
+get_data()
