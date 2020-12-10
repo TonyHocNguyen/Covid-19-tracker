3ffab25520d9fcddf38ce33f8c7bc6259e143372
nguyntu2002 <nguyuntu2002@gmail.com>
2020-12-10 16:13:03 +0700

    web

--- Web auto trial.py
+++ Web auto trial.py
@@ -1,24 +1,32 @@
 from selenium import webdriver
 import time
+
 #from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
-driver.get('https://worldhealthorg.shinyapps.io/covid/')
+driver.get('https://www.cdc.gov/coronavirus/2019-ncov/travelers/map-and-travel-notices.html?fbclid=IwAR1PWJ9ycocek7qcagrm7MqjGczOqvtvlqsp0vOs1-u3AslRRoFeEGZG2Rk')
 
 time.sleep(2)
 
-acceptbutton = driver.find_element_by_xpath('//*[@id="closeModal"]')
-acceptbutton.click()
+#acceptbutton = driver.find_element_by_xpath('//*[@id="closeModal"]')
+#acceptbutton.click()
 
-country_tab = driver.find_element_by_xpath('//*[@id="tabs"]/li[4]/a')
-country_tab.click()
+#country_tab = driver.find_element_by_xpath('//*[@id="tabs"]/li[4]/a')
+#country_tab.click()
 
 country = input(str("Enter the country name: "))
+country_tab = driver.find_element_by_link_text(country)
+country_tab.click()
 
-SearchBox = driver.find_element_by_xpath('//input[@id="singlecountry_header"]/div/div/div/div[1]')
-SearchBox.send_keys(country)
-SearchBox.submit()
+level = (driver.find_element_by_xpath('//*[@id="contentArea"]/div[2]/div[1]/div/div')).text
+print(level)
+
+if 'Low' in level:
+    print('low')
+#SearchBox = driver.find_element_by_xpath('//input[@id="singlecountry_header"]/div/div/div/div[1]')
+#SearchBox.send_keys(country)
+#SearchBox.submit()
 
 #SearchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
 #SearchButton.click()
